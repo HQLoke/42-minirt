e3b92446b87976f67b0c33737f9201eb6ae8b342
Ng Wee Hean <heanconcerto@gmail.com>
2022-05-11 00:10:10 +0800

    Updated test for vector constructor

--- test/test_ft_vec.c
+++ test/test_ft_vec.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 09:32:53 by weng              #+#    #+#             */
-/*   Updated: 2022/05/10 16:19:03 by weng             ###   ########.fr       */
+/*   Updated: 2022/05/11 00:09:08 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 void	test_new(void)
 {
 	t_vec	*vec;
-	size_t	i;
 
 	vec = ft_vec3_new(1, 2.3, 45.6);
-	printf("size = %ld\n", vec->size);
-	i = 0;
-	while (i < vec->size)
-		printf("%f ", vec->data[i++]);
-	printf("\n");
+	if (vec->size != 3
+		|| eq_double(1, vec->data[0]) == 0
+		|| eq_double(2.3, vec->data[1]) == 0
+		|| eq_double(45.6, vec->data[2]) == 0)
+		printf("ft_vec3_new: Error!\n");
+	else
+		printf("ft_vec3_new: OK\n");
 	ft_vec_del(vec);
 }
 
