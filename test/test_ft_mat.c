0bf81a994205d2ee161cf6327946415f77b4cdaa
Ng Wee Hean <heanconcerto@gmail.com>
2022-05-12 16:47:52 +0800

    Simplified matrix tests

--- test/test_ft_mat.c
+++ test/test_ft_mat.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:09:06 by weng              #+#    #+#             */
-/*   Updated: 2022/05/11 17:29:51 by weng             ###   ########.fr       */
+/*   Updated: 2022/05/12 16:46:49 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,45 @@
 
 void	test_empty(void)
 {
-	t_mat	*mat;
-	size_t	row;
-	size_t	col;
-	size_t	i;
+	const double	ptr[] = {0, 0, 0, 0, 0, 0};
+	t_mat			*mat;
+	t_mat			*target;
 
-	row = 2;
-	col = 3;
-	mat = ft_mat_empty(row, col);
-	if (mat->row != row || mat->col != col)
+	mat = ft_mat_empty(2, 3);
+	target = ft_mat_new(2, 3, ptr);
+	if (eq_mat(mat, target) == 0)
 		printf("ft_mat_empty: Error!\n");
 	else
-	{
-		i = 0;
-		while (i < row * col)
-		{
-			if (eq_double(0, mat->data[i / col][i % col]) == 0)
-				printf("ft_mat_empty: Error!\n");
-			++i;
-		}
 		printf("ft_mat_empty: OK\n");
-	}
 	ft_mat_del(mat);
+	ft_mat_del(target);
 }
 
 void	test_new(void)
 {
-	const double	ptr[3] = {1, 2.3, 45.6};
+	const double	ptr[] = {1, 2.3, 45.6, 7, 8, 9.0};
 	t_mat			*mat;
 	size_t			row;
 	size_t			col;
 	size_t			i;
 
 	row = 3;
-	col = 1;
+	col = 2;
 	mat = ft_mat_new(row, col, ptr);
 	if (mat->row != row || mat->col != col)
 		printf("ft_mat_new: Error!\n");
 	else
 	{
-		i = 0;
-		while (i < row * col)
+		i = -1;
+		while (++i < row * col)
 		{
 			if (eq_double(ptr[i], mat->data[i / col][i % col]) == 0)
-				printf("ft_mat_new: Error!\n");
-			++i;
+				break ;
 		}
-		printf("ft_mat_new: OK\n");
+		if (i == row * col)
+			printf("ft_mat_new: OK\n");
+		else
+			printf("ft_mat_new: Error!\n");
 	}
 	ft_mat_del(mat);
 }
@@ -70,52 +62,31 @@ void	test_copy(void)
 	const double	ptr[3] = {1, 2.3, 45.6};
 	t_mat			*mat;
 	t_mat			*cpy;
-	size_t			i;
 
 	mat = ft_mat_new(3, 1, ptr);
 	cpy = ft_mat_copy(mat);
-	if (cpy->row != mat->row || cpy->col != mat->col)
+	if (eq_mat(mat, cpy) == 0)
 		printf("ft_mat_copy: Error!\n");
 	else
-	{
-		i = -1;
-		while (++i < cpy->row * cpy->col)
-		{
-			if (eq_double(ptr[i], cpy->data[i / cpy->col][i % cpy->col]) == 0)
-				printf("ft_mat_copy: Error!\n");
-		}
 		printf("ft_mat_copy: OK\n");
-	}
 	ft_mat_del(mat);
 	ft_mat_del(cpy);
 }
 
 void	test_identity(void)
 {
-	t_mat	*mat;
-	size_t	n;
-	size_t	i;
+	const double	ptr[] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+	t_mat			*mat;
+	t_mat			*target;
 
-	n = 4;
-	mat = ft_mat_identity(n);
-	if (mat->row != n || mat->col != n)
+	mat = ft_mat_identity(4);
+	target = ft_mat_new(4, 4, ptr);
+	if (eq_mat(mat, target) == 0)
 		printf("ft_mat_identity: Error!\n");
 	else
-	{
-		i = -1;
-		while (++i < n * n)
-		{
-			if (!((i / n == i % n && eq_double(1, mat->data[i / n][i % n]) == 1)
-				|| (i / n != i % n
-					&& eq_double(0, mat->data[i / n][i % n]) == 1)))
-			{
-				printf("ft_mat_identity: Error!\n");
-				break ;
-			}
-		}
 		printf("ft_mat_identity: OK\n");
-	}
 	ft_mat_del(mat);
+	ft_mat_del(target);
 }
 
 int	main(void)
--- test/test_ft_mat_mul.c
+++ test/test_ft_mat_mul.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:03:44 by weng              #+#    #+#             */
-/*   Updated: 2022/05/11 23:36:59 by weng             ###   ########.fr       */
+/*   Updated: 2022/05/12 16:34:14 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,58 +15,41 @@
 void	test_scalar(void)
 {
 	const double	ptr[] = {1, 2, 3, 4};
-	size_t			i;
+	const double	ptr2[] = {2, 4, 6, 8};
 	t_mat			*in;
 	t_mat			*out;
+	t_mat			*target;
 
 	in = ft_mat_new(2, 2, ptr);
 	out = ft_mat_mul_scalar(2, in);
-	if (out->row != in->row || out->col != in->col)
+	target = ft_mat_new(2, 2, ptr2);
+	if (eq_mat(out, target) == 0)
 		printf("ft_mat_mul_scalar: Error!\n");
 	else
-	{
-		i = -1;
-		while (++i < out->row * out->col)
-		{
-			if (!eq_double(ptr[i] * 2, out->data[i / out->col][i % out->col]))
-			{
-				printf("ft_mat_mul_scalar: Error!\n");
-				break ;
-			}
-		}
 		printf("ft_mat_mul_scalar: OK\n");
-	}
 	ft_mat_del(in);
 	ft_mat_del(out);
+	ft_mat_del(target);
 }
 
 void	test_elem(void)
 {
 	const double	ptr[] = {1, 2, 3, 4};
-	size_t			i;
+	const double	ptr2[] = {1, 4, 9, 16};
 	t_mat			*in;
 	t_mat			*out;
+	t_mat			*target;
 
 	in = ft_mat_new(2, 2, ptr);
 	out = ft_mat_mul_elem(in, in);
-	if (out->row != in->row || out->col != in->col)
+	target = ft_mat_new(2, 2, ptr2);
+	if (eq_mat(out, target) == 0)
 		printf("ft_mat_mul_elem: Error!\n");
 	else
-	{
-		i = -1;
-		while (++i < out->row * out->col)
-		{
-			if (!eq_double(
-					ptr[i] * ptr[i], out->data[i / out->col][i % out->col]))
-			{
-				printf("ft_mat_mul_elem: Error!\n");
-				break ;
-			}
-		}
 		printf("ft_mat_mul_elem: OK\n");
-	}
 	ft_mat_del(in);
 	ft_mat_del(out);
+	ft_mat_del(target);
 }
 
 void	test_vector(void)
@@ -92,28 +75,26 @@ void	test_vector(void)
 
 void	test_mul(void)
 {
-	const double	ptr[] = {1, 2, 3, 4};
-	const double	ptr2[] = {1, 2, 3, 4, 5, 6};
+	const double	ptr[] = {1, 2, 3, 4, 5, 6};
+	const double	ptr2[] = {7, 8, 9, 10, 11, 12};
+	const double	ptr3[] = {27, 30, 33, 61, 68, 75, 95, 106, 117};
 	t_mat			*a;
 	t_mat			*b;
 	t_mat			*out;
+	t_mat			*target;
 
-	a = ft_mat_new(2, 2, ptr);
+	a = ft_mat_new(3, 2, ptr);
 	b = ft_mat_new(2, 3, ptr2);
 	out = ft_mat_mul(a, b);
-	if (out->row != a->row || out->col != b->col
-		|| eq_double(9, out->data[0][0]) == 0
-		|| eq_double(12, out->data[0][1]) == 0
-		|| eq_double(15, out->data[0][2]) == 0
-		|| eq_double(19, out->data[1][0]) == 0
-		|| eq_double(26, out->data[1][1]) == 0
-		|| eq_double(33, out->data[1][2]) == 0)
+	target = ft_mat_new(3, 3, ptr3);
+	if (eq_mat(out, target) == 0)
 		printf("ft_mat_mul: Error!\n");
 	else
 		printf("ft_mat_mul: OK\n");
 	ft_mat_del(a);
 	ft_mat_del(b);
 	ft_mat_del(out);
+	ft_mat_del(target);
 }
 
 int	main(void)
