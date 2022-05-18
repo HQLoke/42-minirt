f1cd7b5911a08b98f631f2534bd07f7149f39d52
Han qin Loke <hloke@u91z08s03.42kl.edu.my>
2022-05-18 13:54:08 +0800

    ft_atof done. test cases passed

--- src/utils/ft_atof.c
+++ src/utils/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:32:55 by hloke             #+#    #+#             */
-/*   Updated: 2022/05/17 11:18:22 by hloke            ###   ########.fr       */
+/*   Updated: 2022/05/18 13:49:20 by hloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,27 @@ double	ft_atof(const char *str)
 {
 	double	retval;
 	double	tmp;
+	char	*temp;
 	int		len;
-	
+
 	retval = (double)ft_atoi(str);
 	while (*str != '\0' && *str != '.')
 		str += 1;
 	if (*str == '.')
 		str += 1;
-	tmp = (double)ft_atoi(str);
 	len = ft_strlen(str);
+	if (len > 6)
+	{
+		len = 7;
+		temp = ft_substr(str, 0, 7);
+		tmp = (double)ft_atoi(temp);
+		free(temp);
+	}
+	else
+		tmp = (double)ft_atoi(str);
 	while (len--)
 		tmp /= 10;
+	if (retval < 0)
+		return (retval - tmp);
 	return (retval + tmp);
 }
---
+++ test/utils/test_ft_atof.c
@@ -0,0 +1,35 @@
+#include "minirt.h"
+
+void	test_atof(char *str)
+{
+	double	original;
+	double	retval;
+
+	original = atof(str);
+	retval = ft_atof(str);
+	printf("Original is %lf\n  Retval is %lf\n\n", original, retval);
+}
+
+int main(void)
+{
+	test_atof("1");
+	test_atof("999");
+	test_atof("-1");
+	test_atof("-999");
+	test_atof("0.0");
+	test_atof("-0.0");
+	test_atof("9.90000000");
+	test_atof("-9.90000000");
+	test_atof("9.900000500");
+	test_atof("-9.900000500");
+	test_atof("3.14159265359");
+	test_atof("-3.14159265359");
+	test_atof("2147483647.214748");
+	test_atof("-2147483648.214748");
+	test_atof("2147483647.00000000");
+	test_atof("-2147483648.00000000");
+	test_atof("2147483647.2147483647");
+	test_atof("-2147483648.2147483648");
+
+	return (0);
+}
