e36f6ca8fdfda85d64dd305fa9e44e26232a0f63
Tony Loke <hqloke007@gmail.com>
2022-05-24 10:32:17 +0800

    ft_atof 2.0 completed. no norminette check yet

--- src/utils/ft_atof.c
+++ src/utils/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:32:55 by hloke             #+#    #+#             */
-/*   Updated: 2022/05/23 10:52:32 by hloke            ###   ########.fr       */
+/*   Updated: 2022/05/24 10:29:46 by hloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,48 @@
 /*
 Converts string to floating number
 */
-double	ft_atof(const char *str)
+double	ft_atof(const char *nptr)
 {
 	double	retval;
-	double	tmp;
-	int		len;
+	int		decimal;
+	int		sign;
 
-	retval = (double)ft_atol(str);
-	while (*str != '\0' && *str != '.')
-		str += 1;
-	if (*str == '.')
-		str += 1;
-	len = ft_strlen(str);
-	tmp = (double)ft_atol(str);
-	while (len--)
-		tmp /= 10;
-	if (retval < 0)
-		return (retval - tmp);
-	return (retval + tmp);
+	retval = 0;
+	sign = 1;
+	if (*nptr == '-')
+	{
+		sign = -1;
+		nptr++;
+	}
+	while (*nptr != '\0' && *nptr != '.')
+	{
+		retval *= 10;
+		retval += *nptr - '0';
+		nptr += 1;
+	}
+	retval *= sign;
+	if (*nptr == '.')
+		nptr += 1;
+	decimal = 1;
+	while (ft_isdigit(*nptr) == 1)
+	{
+		if (retval > 0)
+			retval += ((*nptr - '0') / pow(10, decimal));
+		else
+			retval -= ((*nptr - '0') / pow(10, decimal));
+		decimal += 1;
+		nptr += 1;
+	}
+	if (*nptr == 'e')
+		nptr += 1;
+	int mul = 0;
+	while (ft_isdigit(*nptr) == 1)
+	{
+		mul *= 10;
+		mul += *nptr - '0';
+		nptr += 1;
+	}
+	while (mul-- > 0)
+		retval *= 10;
+	return (retval);
 }
