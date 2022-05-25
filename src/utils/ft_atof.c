cb6abaee3d191ec45a63200bd8db098ded00fc5e
Ng Wee Hean <heanconcerto@gmail.com>
2022-05-25 15:27:29 +0800

    Debug and rewritten ft_atof

--- src/utils/ft_atof.c
+++ src/utils/ft_atof.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atof.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:32:55 by hloke             #+#    #+#             */
-/*   Updated: 2022/05/24 10:29:46 by hloke            ###   ########.fr       */
+/*   Updated: 2022/05/25 15:23:23 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+/* Handles the integral component of a number as a double, and moves
+ * the pointer to the unhandled part of the string. */
+static double	ft_atof_integral(const char **nptr)
+{
+	double	retval;
+	int		sign;
+
+	retval = 0;
+	sign = 1;
+	if (**nptr == '-')
+		sign = -1;
+	if (**nptr == '+' || **nptr == '-')
+		++(*nptr);
+	while (ft_isdigit(**nptr) == 1)
+	{
+		retval = retval * 10 + (**nptr - '0');
+		++(*nptr);
+	}
+	return (sign * retval);
+}
 
 /*
 Converts string to floating number
@@ -18,45 +39,26 @@ Converts string to floating number
 double	ft_atof(const char *nptr)
 {
 	double	retval;
-	int		decimal;
+	double	decimal;
 	int		sign;
 
-	retval = 0;
 	sign = 1;
 	if (*nptr == '-')
-	{
 		sign = -1;
-		nptr++;
-	}
-	while (*nptr != '\0' && *nptr != '.')
-	{
-		retval *= 10;
-		retval += *nptr - '0';
-		nptr += 1;
-	}
-	retval *= sign;
+	if (*nptr == '+' || *nptr == '-')
+		++nptr;
+	retval = ft_atof_integral(&nptr);
 	if (*nptr == '.')
-		nptr += 1;
-	decimal = 1;
-	while (ft_isdigit(*nptr) == 1)
 	{
-		if (retval > 0)
-			retval += ((*nptr - '0') / pow(10, decimal));
-		else
-			retval -= ((*nptr - '0') / pow(10, decimal));
-		decimal += 1;
-		nptr += 1;
+		decimal = 0.1;
+		while (ft_isdigit(*(++nptr)) == 1)
+		{
+			retval += (*nptr - '0') * decimal;
+			decimal /= 10;
+		}
 	}
-	if (*nptr == 'e')
-		nptr += 1;
-	int mul = 0;
-	while (ft_isdigit(*nptr) == 1)
-	{
-		mul *= 10;
-		mul += *nptr - '0';
-		nptr += 1;
-	}
-	while (mul-- > 0)
-		retval *= 10;
-	return (retval);
+	if (*nptr++ == 'e')
+		return (sign * retval * pow(10, ft_atof_integral(&nptr)));
+	else
+		return (sign * retval);
 }
