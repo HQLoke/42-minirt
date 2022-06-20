8ef50571897240e3213691cd0b9a06017d69bd94
Ng Wee Hean <heanconcerto@gmail.com>
2022-06-20 13:59:59 +0800

    Corrected transformation operations on surface objects

--- src/display/ft_obj_transform.c
+++ src/display/ft_obj_transform.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_obj_transform.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 13:45:02 by hloke             #+#    #+#             */
-/*   Updated: 2022/06/19 19:14:12 by hloke            ###   ########.fr       */
+/*   Updated: 2022/06/20 13:51:15 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	ft_obj_rotate_x(t_obj *obj, double theta)
 	if (obj == NULL)
 		return ;
 	transform = ft_affine_rotate_x(theta);
-	ft_mat_mul(obj->to_world, transform);
+	transform = ft_mat_mul(transform, obj->to_world);
+	ft_mat_swap(obj->to_world, transform);
 	ft_mat_del(transform);
 	ft_mat_del(obj->fr_world);
 	obj->fr_world = ft_mat_affine_inverse(ft_mat_copy(obj->to_world));
@@ -32,7 +33,8 @@ void	ft_obj_rotate_y(t_obj *obj, double theta)
 	if (obj == NULL)
 		return ;
 	transform = ft_affine_rotate_y(theta);
-	ft_mat_mul(obj->to_world, transform);
+	transform = ft_mat_mul(transform, obj->to_world);
+	ft_mat_swap(obj->to_world, transform);
 	ft_mat_del(transform);
 	ft_mat_del(obj->fr_world);
 	obj->fr_world = ft_mat_affine_inverse(ft_mat_copy(obj->to_world));
@@ -45,7 +47,8 @@ void	ft_obj_rotate_z(t_obj *obj, double theta)
 	if (obj == NULL)
 		return ;
 	transform = ft_affine_rotate_z(theta);
-	ft_mat_mul(obj->to_world, transform);
+	transform = ft_mat_mul(transform, obj->to_world);
+	ft_mat_swap(obj->to_world, transform);
 	ft_mat_del(transform);
 	ft_mat_del(obj->fr_world);
 	obj->fr_world = ft_mat_affine_inverse(ft_mat_copy(obj->to_world));
@@ -58,7 +61,8 @@ void	ft_obj_scale(t_obj *obj, double scale)
 	if (obj == NULL)
 		return ;
 	transform = ft_affine_scale(scale);
-	ft_mat_mul(obj->to_world, transform);
+	transform = ft_mat_mul(transform, obj->to_world);
+	ft_mat_swap(obj->to_world, transform);
 	ft_mat_del(transform);
 	ft_mat_del(obj->fr_world);
 	obj->fr_world = ft_mat_affine_inverse(ft_mat_copy(obj->to_world));
@@ -71,7 +75,8 @@ void	ft_obj_translate(t_obj *obj, double dx, double dy, double dz)
 	if (obj == NULL)
 		return ;
 	transform = ft_affine_translate(dx, dy, dz);
-	ft_mat_mul(obj->to_world, transform);
+	transform = ft_mat_mul(transform, obj->to_world);
+	ft_mat_swap(obj->to_world, transform);
 	ft_mat_del(transform);
 	ft_mat_del(obj->fr_world);
 	obj->fr_world = ft_mat_affine_inverse(ft_mat_copy(obj->to_world));
