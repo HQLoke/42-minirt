ca1c531000f2649d7c56f83a8765ea8ae045974d
Ng Wee Hean <heanconcerto@gmail.com>
2022-06-20 13:36:04 +0800

    Bug removal: memory leak when calling ft_display_update

--- src/display/ft_display.c
+++ src/display/ft_display.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_display.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hloke <hloke@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:22:27 by hloke             #+#    #+#             */
-/*   Updated: 2022/06/20 11:16:20 by hloke            ###   ########.fr       */
+/*   Updated: 2022/06/20 13:32:13 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_display_update(t_mlx *mlx)
 {
 	t_img	*img;
 
-	img = ft_render(mlx->cam, mlx->ambient, mlx->lights, \
-	mlx->objs);
+	img = ft_render(mlx->cam, mlx->ambient, mlx->lights, mlx->objs);
+	ft_image8_del(mlx->img8);
 	mlx->img8 = ft_image_2_image8(img);
 	ft_image_del(img);
 	ft_draw_image(mlx);
