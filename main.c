5a616cde75c2ea5f6660b05b042c9395ce1ff3a8
Ng Wee Hean <heanconcerto@gmail.com>
2022-06-22 17:21:09 +0800

    Simplified main.c to call ft_perror upon invalid argc

--- main.c
+++ main.c
@@ -6,19 +6,13 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 10:27:23 by hloke             #+#    #+#             */
-/*   Updated: 2022/06/22 16:55:28 by weng             ###   ########.fr       */
+/*   Updated: 2022/06/22 17:13:01 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-static int	teach_them(void)
-{
-	printf("Usage: ./minirt [scene file in format *.rt]\n");
-	return (1);
-}
 
 /* Run minilibx */
 int	main(int argc, char **argv)
@@ -27,7 +21,7 @@ int	main(int argc, char **argv)
 
 	errno = 0;
 	if (argc != 2)
-		return (teach_them());
+		ft_perror("Usage: ./minirt [scene file in format *.rt]", EINVAL);
 	mlx = ft_mlx_new(400, 300);
 	mlx->cam = ft_parse_scene(argv[1], &mlx->ambient, &mlx->lights, &mlx->objs);
 	mlx->image = ft_mlx_image_new(mlx->mlx_ptr, 1920, 1080);
