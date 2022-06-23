637e3ea4ca8c3a073f3de9eec53a8cd7e718e1b2
Ng Wee Hean <heanconcerto@gmail.com>
2022-06-23 22:44:18 +0800

    String display on screen is moved to (0, 20)

--- src/display/ft_display.c
+++ src/display/ft_display.c
@@ -6,22 +6,19 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:22:27 by hloke             #+#    #+#             */
-/*   Updated: 2022/06/23 22:22:28 by weng             ###   ########.fr       */
+/*   Updated: 2022/06/23 22:43:48 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "display.h"
 
 /* 
- * Display string at coordinates (0,0).
+ * Display string at coordinates (10, 20).
  * Coordinates are not included in the argument for simplication.
  */
 void	ft_display_string(t_mlx *mlx, int color, const char *str)
 {
-	char	*output;
-
-	output = (char *)str;
-	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 0, 0, color, output);
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 10, 20, color, (char *) str);
 }
 
 void	ft_display_update(t_mlx *mlx)
